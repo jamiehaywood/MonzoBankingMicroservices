3b557ec1d7d4bf7d18ddf0c8ac3eff3e56424715
Jamie Haywood <jamie.haywood@gmail.com>
2019-06-01 13:10:02 +0100

    modified indexjs to be very basic for tests

--- TokenRefresh/index.js
+++ TokenRefresh/index.js
@@ -1,16 +1,15 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.body) {
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            status: 200
         };
     }
+
     else {
         context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            status: 400
         };
     }
 };
