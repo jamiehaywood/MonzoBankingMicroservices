eb70150f8be25f6fc64abd8d572d51fabe110660
Jamie Haywood <jamie.haywood@gmail.com>
2019-06-01 13:38:29 +0100

    added connection to azure table storage

--- TokenRefresh/index.js
+++ TokenRefresh/index.js
@@ -1,15 +1,50 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    var azure = require('azure-storage');
+    var request = require("request");
+    var entGen = azure.TableUtilities.entityGenerator;
+
+    var tableService = azure.createTableService(process.env["storageName"], process.env["storageAccessKey"])
 
     if (req.body) {
-        context.res = {
-            status: 200
-        };
-    }
 
-    else {
-        context.res = {
-            status: 400
+        // Fetch existing credentials from the storage table
+        export async function credentialsRetriever() {
+
+            var query = new azure.TableQuery().top(5);
+
+            return new Promise((res, rej) => {
+                tableService.queryEntities('credentials', query, null, function (error, result, response) {
+
+                    if (!error) {
+                        var credentialObject = {};
+                        response.body.value.map(item => {
+                            credentialObject[item.RowKey] = item.value
+                        })
+                        res(credentialObject)
+                        console.log("Credentials successfully retrieved from table")
+                    }
+                    else {
+                        rej(error)
+                    }
+
+                })
+            })
         };
+
+        let existingCredentials = await credentialsRetriever()
+
+        if (existingCredentials) {
+            context.res = {
+                status: 200,
+                body: existingCredentials
+            }
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: "houston, we've had a problem."
+            };
+
+        }
     }
-};
+}
