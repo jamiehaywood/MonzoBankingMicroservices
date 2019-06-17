8ff74a1252dac72e5a632f52a4a1b45c09a716cb
Jamie Haywood <jamie.haywood@gmail.com>
2019-06-17 17:43:39 +0100

    updating getStocks

--- StockChecker/index.js
+++ StockChecker/index.js
@@ -1,42 +1,17 @@
 module.exports = async function (context, myTimer) {
-    const stocks = require('./stocks.json')
-    var stockTickers = stocks.map(x => x.stockopediaTicker)
-    var request = require("request");
+    const getStockQuotes = require('./getStockQuotes');
+    const bankHolidayCheck = require('./bankHolidayCheck');
 
-    const todaysQuotes = async () => {
-        let promiseArray = []
-        stockTickers.forEach(ticker => {
-            var options = {
-                method: 'GET',
-                url: 'https://www.stockopedia.com/ajax/get_prices/' + ticker + '/'
-            };
-            new Promise(function (res, rej) {
-                request(options, function (error, response, body) {
-                    let quotesObject = {}
-                    rej(error)
-                    quotesObject[ticker] = body.pop().pop()
-                    promiseArray.push(res(quotesObject))
-                });
-            })
-        });
-        return await promiseArray
+
+    const bankHoliday = await bankHolidayCheck()
+    if (!bankHoliday) {
+        const quotes = getStockQuotes()
+        console.log(await quotes)
     }
-
-    Promise.all(todaysQuotes).then(
-        console.log(todaysQuotes)
-    )
 }
-    //     const bankHolidayCheck = require('./bankHolidayCheck');
-//     const getTotalDayChange = require('./getTotalDayChange');
 //     const getTotalMonthChange = require('./getTotalMonthChange');
 //     const getTotalValueChange = require('./getTotalValueChange');
 
-//     const bankHoliday = await bankHolidayCheck()
-
-//     if(!bankHoliday){
-//         const value = await getTotalValueChange()
-//         console.log(value)
-//     }
 //     else{
 //         context.log("its a bank holiday!")
 //         context.done()
