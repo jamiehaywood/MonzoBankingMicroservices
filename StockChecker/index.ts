a47153587289618b4272db3313abe94a8b2aef63
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-06 15:46:05 +0100

    added getPercentageChanges to index

--- StockChecker/index.ts
+++ StockChecker/index.ts
@@ -1,10 +1,13 @@
 import { AzureFunction, Context } from "@azure/functions"
 import bankHolidayCheck from "./bankHolidayCheck"
 import getStockQuotes from "./getStockQuotes";
+import getPercentageChanges from "./getPercentageChanges";
 
 const timerTrigger: AzureFunction = async function (): Promise<void> {
     if (await bankHolidayCheck() === false) {
         const quotes = await getStockQuotes()
+        const percentageChanges = await getPercentageChanges(quotes)
+        console.log(percentageChanges)
     }
     
 
