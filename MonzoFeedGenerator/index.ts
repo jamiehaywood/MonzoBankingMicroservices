88962bbaed084b96da530dd7c0013cc677ca89e4
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-07 12:33:08 +0100

    Initial commit of Feed Generator

---
+++ MonzoFeedGenerator/index.ts
@@ -0,0 +1,21 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name:IRequestBody = (req.body);
+
+    if (name) {
+        context.res = {
+            status: 202, // for an async response.
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
+
+export default httpTrigger;
---
+++ MonzoFeedGenerator/interface.d.ts
@@ -0,0 +1,9 @@
+interface IRequestBody{
+    url?:string,
+    title:string,
+    imageUrl:string,
+    backgroundColor?:string,
+    bodyColor?:string,
+    titleColor?:string,
+    bodyText?:string
+}
