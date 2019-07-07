707edbc41d795921e554fd5becfe553835085bdb
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-07 15:22:18 +0100

    added IRefreshedCredentials interface

--- TokenRefresh/interface.d.ts
+++ TokenRefresh/interface.d.ts
@@ -4,3 +4,13 @@ interface ICredentialsObject {
     clientSecret: string
     refreshToken: string
 }
+
+interface IRefreshedCredentials {
+    access_token: string,
+    client_id: string,
+    expires_in: number,
+    refresh_token: string,
+    scope: string,
+    token_type: string,
+    user_id: string
+}
