c5a11e09102c60a2ed352b2d6b6a7f92c4f191f9
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-08 13:24:43 +0100

    added the POST request for the monzo feed

--- MonzoFeedGenerator/index.ts
+++ MonzoFeedGenerator/index.ts
@@ -1,10 +1,42 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import request from 'request'
+import credentialsRetriever from '../TokenRefresh/credentialsRetriever'
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name:IRequestBody = (req.body);
+const MonzoFeedGenerator: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const body: IRequestBody = (req.body);
 
-    if (name) {
+    if (body) {
+        let credentials = await credentialsRetriever()
+        let accessToken = credentials.accessToken
+
+        var options = {
+            method: 'POST',
+            url: 'https://api.monzo.com/feed',
+            headers:
+            {
+                Authorization: accessToken,
+                "Content-Type": 'application/x-www-form-urlencoded'
+            },
+            form:
+            {
+                account_id: credentials.accountId,
+                type: credentials.accountType,
+                url: body.url,
+                'params[title]': body.title,
+                'params[image_url]': body.imageUrl,
+                'params[background_color]': body.backgroundColor,
+                'params[body_color]': body.bodyColor,
+                'params[title_color]': body.titleColor,
+                'params[body]': body.bodyText
+            }
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+
+            console.log(body);
+        });
+
         context.res = {
             status: 202, // for an async response.
             body: "Hello " + (req.query.name || req.body.name)
@@ -18,4 +50,4 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     }
 };
 
-export default httpTrigger;
+export default MonzoFeedGenerator;
