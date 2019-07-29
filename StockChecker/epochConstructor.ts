f1d52a5d9dd750a7b123a09f65d5644870666e9c
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-29 20:53:50 +0100

    epoch logic fix if days land on sat or sun

--- StockChecker/epochConstructor.ts
+++ StockChecker/epochConstructor.ts
@@ -1,7 +1,17 @@
 import moment from 'moment';
 
-const epochConstructor = function (daysAgo:number) : number {
-    return moment().subtract(daysAgo, 'days').hours(12).minutes(0).seconds(0).millisecond(0).valueOf();
+const epochConstructor = function (daysAgo: number): number {
+    let input = moment().subtract(daysAgo, 'days').hours(12).minutes(0).seconds(0).millisecond(0)
+    switch (input.day()) {
+        case 0: //sunday
+            return input.subtract(2, 'days').valueOf()
+        case 6: //saturday
+            return input.subtract(1, 'days').valueOf()
+        case 1: //monday
+            return input.subtract(3, 'days').valueOf()
+        default:
+            return input.valueOf()
+    }
 }
 
 export default epochConstructor
