b15cafec105b5a7bc3d07c61789bcdd462a2587e
Jamie Haywood <jamie.haywood@gmail.com>
2019-08-07 17:20:31 +0100

    added tests for epochConstructor

---
+++ __tests__/epochConstructor.test.ts
@@ -0,0 +1,17 @@
+import epochConstructor from '../StockChecker/epochConstructor'
+import moment from 'moment'
+
+test('if Sat 6th July 2019 is input, epoch returned will be previous Friday at noon', () => {
+  let today = moment("06-07-2019", 'DD-MM-YYYY')
+  expect(epochConstructor(today, 1)).toBe(1562324400000);
+});
+
+test('if Sun 7th July 2019 is input, epoch returned will be previous Friday at noon', () => {
+  let today = moment("07-07-2019", 'DD-MM-YYYY')
+  expect(epochConstructor(today, 1)).toBe(1562324400000);
+});
+
+test('if Mon 8th July 2019 is input, epoch returned will be previous Friday at noon', () => {
+  let today = moment("07-07-2019", 'DD-MM-YYYY')
+  expect(epochConstructor(today, 1)).toBe(1562324400000);
+});
